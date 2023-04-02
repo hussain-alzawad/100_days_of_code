6314018a3976929b41d5309ee8174355713f0c52
Hussain Al Zawad <haz1430@hotmail.com>
2023-04-02 05:37:03 +0300

    added substraction method

--- main.py
+++ main.py
@@ -4,9 +4,13 @@
 def sum(a,b):
     return a+b
 
+def substraction(a,b):
+    return a-b
 
  
 a = int(input("enter the first number:" ))
 b = int(input("enter the second number: "))
 
 print(sum(a,b))
+
+print(substraction(a,b))
